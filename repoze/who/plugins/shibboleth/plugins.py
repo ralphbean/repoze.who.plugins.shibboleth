09c72a91cf872edf3113c6ac117a985230e1e848
Ralph Bean <ralph.bean@gmail.com>
2010-10-13 15:35:59 -0400

    refactoring (rename)

--- repoze/who/plugins/shibboleth/plugins.py
+++ repoze/who/plugins/shibboleth/plugins.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 #
+# repoze.who.plugins.shibboleth, Shibboleth authentication for WSGI applications.
+# Copyright (C) 2010 by Ralph Bean <http://threebean.wordpress.com/>
+#
+# Forked from:
 # repoze.who.plugins.ldap, LDAP authentication for WSGI applications.
 # Copyright (C) 2010 by Gustavo Narea  <http://gustavonarea.net/> and
 #                       Lorenzo M. Catucci <http://www.uniroma2.it/>.
 # Copyright (C) 2008 by Gustavo Narea <http://gustavonarea.net/>.
-#
-# This file is part of repoze.who.plugins.ldap
-# <http://code.gustavonarea.net/repoze.who.plugins.ldap/>
 #
 # This software is subject to the provisions of the BSD-like license at
 # http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
@@ -14,10 +15,10 @@
 # EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
 # THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
 # FITNESS FOR A PARTICULAR PURPOSE.
-"""LDAP plugins for repoze.who."""
+"""Shibboleth plugins for repoze.who."""
 
-__all__ = ['LDAPBaseAuthenticatorPlugin', 'LDAPAuthenticatorPlugin',
-           'LDAPSearchAuthenticatorPlugin', 'LDAPAttributesPlugin']
+__all__ = ['ShibbolethBaseAuthenticatorPlugin', 'ShibbolethAuthenticatorPlugin',
+           'ShibbolethSearchAuthenticatorPlugin', 'ShibbolethAttributesPlugin']
 
 from zope.interface import implements
 import ldap
@@ -32,15 +33,15 @@ import re
 #{ Authenticators
 
 
-class LDAPBaseAuthenticatorPlugin(object):
+class ShibbolethBaseAuthenticatorPlugin(object):
 
     implements(IAuthenticator)
 
     def __init__(self, ldap_connection, base_dn, returned_id='dn',
                  start_tls=False, bind_dn='', bind_pass='', **kwargs):
-        """Create an LDAP authentication plugin.
+        """Create an Shibboleth authentication plugin.
         
-        By passing an existing LDAPObject, you're free to use the LDAP
+        By passing an existing ShibbolethObject, you're free to use the Shibboleth
         authentication method you want, the way you want.
    
         This is an *abstract* class, which means it's useless in itself. You
@@ -50,8 +51,8 @@ class LDAPBaseAuthenticatorPlugin(object):
         This plugin is compatible with any identifier plugin that defines the
         C{login} and C{password} items in the I{identity} dictionary.
         
-        @param ldap_connection: An initialized LDAP connection.
-        @type ldap_connection: C{ldap.ldapobject.SimpleLDAPObject}
+        @param ldap_connection: An initialized Shibboleth connection.
+        @type ldap_connection: C{ldap.ldapobject.SimpleShibbolethObject}
 
         @param base_dn: The base for the I{Distinguished Name}. Something like
             C{ou=employees,dc=example,dc=org}, to which will be prepended the
@@ -128,7 +129,7 @@ class LDAPBaseAuthenticatorPlugin(object):
 
         if not hasattr(self.ldap_connection, 'simple_bind_s'):
             environ['repoze.who.logger'].warn('Cannot bind with the provided '
-                                              'LDAP connection object')
+                                              'Shibboleth connection object')
             return None
         
         try:
@@ -140,27 +141,27 @@ class LDAPBaseAuthenticatorPlugin(object):
             else:
                 identity['userdata'] = userdata + '<dn:%s>' % b64encode(dn)
                 return identity['login']
-        except ldap.LDAPError:
+        except ldap.ShibbolethError:
             return None
 
     def __repr__(self):
         return '<%s %s>' % (self.__class__.__name__, id(self))
 
 
-class LDAPAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
+class ShibbolethAuthenticatorPlugin(ShibbolethBaseAuthenticatorPlugin):
 
     def __init__(self, ldap_connection, base_dn, naming_attribute='uid',
                  **kwargs):
-        """Create an LDAP authentication plugin using pattern-determined DNs
+        """Create an Shibboleth authentication plugin using pattern-determined DNs
         
-        By passing an existing LDAPObject, you're free to use the LDAP
+        By passing an existing ShibbolethObject, you're free to use the Shibboleth
         authentication method you want, the way you want.
     
         This plugin is compatible with any identifier plugin that defines the
         C{login} and C{password} items in the I{identity} dictionary.
         
-        @param ldap_connection: An initialized LDAP connection.
-        @type ldap_connection: C{ldap.ldapobject.SimpleLDAPObject}
+        @param ldap_connection: An initialized Shibboleth connection.
+        @type ldap_connection: C{ldap.ldapobject.SimpleShibbolethObject}
         @param base_dn: The base for the I{Distinguished Name}. Something like
             C{ou=employees,dc=example,dc=org}, to which will be prepended the
             user id: C{uid=jsmith,ou=employees,dc=example,dc=org}.
@@ -172,7 +173,7 @@ class LDAPAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
         @raise ValueError: If at least one of the parameters is not defined.
 
         The following parameters are inherited from 
-        L{LDAPBaseAuthenticatorPlugin.__init__}
+        L{ShibbolethBaseAuthenticatorPlugin.__init__}
         @param base_dn: The base for the I{Distinguished Name}. Something like
             C{ou=employees,dc=example,dc=org}, to which will be prepended the
             user id: C{uid=jsmith,ou=employees,dc=example,dc=org}.
@@ -185,7 +186,7 @@ class LDAPAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
         
 
         """
-        LDAPBaseAuthenticatorPlugin.__init__(self, ldap_connection, base_dn,
+        ShibbolethBaseAuthenticatorPlugin.__init__(self, ldap_connection, base_dn,
                                              **kwargs)
         self.naming_pattern = u'%s=%%s,%%s' % naming_attribute
 
@@ -209,7 +210,7 @@ class LDAPAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
         if self.bind_dn:
             try:
                 self.ldap_connection.bind_s(self.bind_dn, self.bind_password)
-            except ldap.LDAPError:
+            except ldap.ShibbolethError:
                 raise ValueError("Couldn't bind with supplied credentials")
         try:
             return self.naming_pattern % ( identity['login'], self.base_dn)
@@ -217,20 +218,20 @@ class LDAPAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
             raise ValueError
 
 
-class LDAPSearchAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
+class ShibbolethSearchAuthenticatorPlugin(ShibbolethBaseAuthenticatorPlugin):
 
     def __init__(self, ldap_connection, base_dn, naming_attribute='uid',
                  search_scope='subtree', restrict='', **kwargs):
-        """Create an LDAP authentication plugin determining the DN via LDAP searches.
+        """Create an Shibboleth authentication plugin determining the DN via Shibboleth searches.
         
-        By passing an existing LDAPObject, you're free to use the LDAP
+        By passing an existing ShibbolethObject, you're free to use the Shibboleth
         authentication method you want, the way you want.
     
         This plugin is compatible with any identifier plugin that defines the
         C{login} and C{password} items in the I{identity} dictionary.
         
-        @param ldap_connection: An initialized LDAP connection.
-        @type ldap_connection: C{ldap.ldapobject.SimpleLDAPObject}
+        @param ldap_connection: An initialized Shibboleth connection.
+        @type ldap_connection: C{ldap.ldapobject.SimpleShibbolethObject}
         @param base_dn: The base for the I{Distinguished Name}. Something like
             C{ou=employees,dc=example,dc=org}, to which will be prepended the
             user id: C{uid=jsmith,ou=employees,dc=example,dc=org}.
@@ -251,7 +252,7 @@ class LDAPSearchAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
         @raise ValueError: If at least one of the parameters is not defined.
 
         The following parameters are inherited from 
-        L{LDAPBaseAuthenticatorPlugin.__init__}
+        L{ShibbolethBaseAuthenticatorPlugin.__init__}
         @param base_dn: The base for the I{Distinguished Name}. Something like
             C{ou=employees,dc=example,dc=org}, to which will be prepended the
             user id: C{uid=jsmith,ou=employees,dc=example,dc=org}.
@@ -263,7 +264,7 @@ class LDAPSearchAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
         @param bind_pass: The password for bind_dn directory entry
         
         """
-        LDAPBaseAuthenticatorPlugin.__init__(self, ldap_connection, base_dn,
+        ShibbolethBaseAuthenticatorPlugin.__init__(self, ldap_connection, base_dn,
                                              **kwargs)
 
         if search_scope[:3].lower() == 'sub':
@@ -301,7 +302,7 @@ class LDAPSearchAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
         if self.bind_dn:
             try:
                 self.ldap_connection.bind_s(self.bind_dn, self.bind_password)
-            except ldap.LDAPError:
+            except ldap.ShibbolethError:
                 raise ValueError("Couldn't bind with supplied credentials")
         try:
             login_name = identity['login'].replace('*',r'\*')
@@ -318,15 +319,15 @@ class LDAPSearchAuthenticatorPlugin(LDAPBaseAuthenticatorPlugin):
                 raise ValueError('Too many entries found for %s' % srch)
             else:
                 raise ValueError('No entry found for %s' %srch)
-        except (KeyError, TypeError, ldap.LDAPError):
+        except (KeyError, TypeError, ldap.ShibbolethError):
             raise # ValueError
 
 
 #{ Metadata providers
 
 
-class LDAPAttributesPlugin(object):
-    """Loads LDAP attributes of the authenticated user."""
+class ShibbolethAttributesPlugin(object):
+    """Loads Shibboleth attributes of the authenticated user."""
     
     implements(IMetadataProvider)
 
@@ -336,11 +337,11 @@ class LDAPAttributesPlugin(object):
                  filterstr='(objectClass=*)', start_tls='',
                  bind_dn='', bind_pass=''):
         """
-        Fetch LDAP attributes of the authenticated user.
+        Fetch Shibboleth attributes of the authenticated user.
         
-        @param ldap_connection: The LDAP connection to use to fetch this data.
-        @type ldap_connection: C{ldap.ldapobject.SimpleLDAPObject} or C{str}
-        @param attributes: The authenticated user's LDAP attributes you want to
+        @param ldap_connection: The Shibboleth connection to use to fetch this data.
+        @type ldap_connection: C{ldap.ldapobject.SimpleShibbolethObject} or C{str}
+        @param attributes: The authenticated user's Shibboleth attributes you want to
             use in your application; an interable or a comma-separate list of
             attributes in a string, or C{None} to fetch them all.
         @type attributes: C{iterable} or C{str}
@@ -360,7 +361,7 @@ class LDAPAttributesPlugin(object):
             iterable.
 
         The following parameters are inherited from 
-        L{LDAPBaseAuthenticatorPlugin.__init__}
+        L{ShibbolethBaseAuthenticatorPlugin.__init__}
         @param base_dn: The base for the I{Distinguished Name}. Something like
             C{ou=employees,dc=example,dc=org}, to which will be prepended the
             user id: C{uid=jsmith,ou=employees,dc=example,dc=org}.
@@ -378,7 +379,7 @@ class LDAPAttributesPlugin(object):
             # Converted to list, just in case...
             attributes = list(attributes)
         elif attributes is not None:
-            raise ValueError('The needed LDAP attributes are not valid')
+            raise ValueError('The needed Shibboleth attributes are not valid')
         self.ldap_connection = make_ldap_connection(ldap_connection)
         if start_tls:
             try:
@@ -417,11 +418,11 @@ class LDAPAttributesPlugin(object):
         if self.bind_dn:
             try:
                 self.ldap_connection.bind_s(self.bind_dn, self.bind_pass)
-            except ldap.LDAPError:
+            except ldap.ShibbolethError:
                 raise ValueError("Couldn't bind with supplied credentials")
         try:
             attributes = self.ldap_connection.search_s(*args)
-        except ldap.LDAPError, msg:
+        except ldap.ShibbolethError, msg:
             environ['repoze.who.logger'].warn('Cannot add metadata: %s' % msg)
             raise Exception(identity)
         else:
@@ -432,25 +433,25 @@ class LDAPAttributesPlugin(object):
 
 
 def make_ldap_connection(ldap_connection):
-    """Return an LDAP connection object to the specified server.
+    """Return an Shibboleth connection object to the specified server.
     
-    If the C{ldap_connection} is already an LDAP connection object, it will
-    be returned as is. If it's an LDAP URL, it will return an LDAP connection
-    to the LDAP server specified in the URL.
+    If the C{ldap_connection} is already an Shibboleth connection object, it will
+    be returned as is. If it's an Shibboleth URL, it will return an Shibboleth connection
+    to the Shibboleth server specified in the URL.
     
-    @param ldap_connection: The LDAP connection object or the LDAP URL of the
+    @param ldap_connection: The Shibboleth connection object or the Shibboleth URL of the
         server to be connected to.
-    @type ldap_connection: C{ldap.ldapobject.SimpleLDAPObject}, C{str} or
+    @type ldap_connection: C{ldap.ldapobject.SimpleShibbolethObject}, C{str} or
         C{unicode}
-    @return: The LDAP connection object.
-    @rtype: C{ldap.ldapobject.SimpleLDAPObject}
+    @return: The Shibboleth connection object.
+    @rtype: C{ldap.ldapobject.SimpleShibbolethObject}
     @raise ValueError: If C{ldap_connection} is C{None}.
     
     """
     if isinstance(ldap_connection, str) or isinstance(ldap_connection, unicode):
         return ldap.initialize(ldap_connection)
     elif ldap_connection is None:
-        raise ValueError('An LDAP connection must be specified')
+        raise ValueError('An Shibboleth connection must be specified')
     return ldap_connection
 
 
